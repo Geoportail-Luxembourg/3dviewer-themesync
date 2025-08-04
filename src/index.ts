18a83cab612a234b4e579c2bf3ab8f46cd3b0bda
Tobias Kohr <tobias.kohr@camptocamp.com>
2025-08-04 14:29:06 +0200

    feat(init): fetch themes and map first layers to layers and contentTree

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import type { VcsPlugin, VcsUiApp, PluginConfigEditor } from '@vcmap/ui';
+import { VcsModule, VcsModuleConfig } from '@vcmap/core';
 import { name, version, mapVersion } from '../package.json';
+
+// TODO: move to plugin config
+const LUX_THEMES_URL =
+  'https://migration.geoportail.lu/themes?limit=30&partitionlimit=5&interface=main&cache_version=500844b6967f4c4b9f05085f3c22da5c&background=background';
+const LUX_OWS_URL = 'https://wmsproxy.geoportail.lu/ogcproxywms';
+const LUX_WMTS_URL = 'https://wmts3.geoportail.lu/mapproxy_4_v3/wmts';
+const MODULE_ID = 'catalogConfig';
 
 type PluginConfig = Record<never, never>;
 type PluginState = Record<never, never>;
 
-type MyPlugin = VcsPlugin<PluginConfig, PluginState>;
+type CatalogPlugin = VcsPlugin<PluginConfig, PluginState>;
 
 export default function plugin(
   config: PluginConfig,
   baseUrl: string,
-): MyPlugin {
+): CatalogPlugin {
   // eslint-disable-next-line no-console
   console.log(config, baseUrl);
   return {
@@ -22,13 +30,103 @@ export default function plugin(
     get mapVersion(): string {
       return mapVersion;
     },
-    initialize(vcsUiApp: VcsUiApp, state?: PluginState): Promise<void> {
+    async initialize(vcsUiApp: VcsUiApp, state?: PluginState): Promise<void> {
       // eslint-disable-next-line no-console
       console.log(
         'Called before loading the rest of the current context. Passed in the containing Vcs UI App ',
         vcsUiApp,
         state,
       );
+
+      const themes = await fetch(LUX_THEMES_URL).then((response) =>
+        response.json(),
+      );
+      console.log('Fetched themes:', themes);
+
+      const module = vcsUiApp.getModuleById(MODULE_ID);
+      // FIXME: contentTree should be in VcsModuleConfig type
+      const appConfig: VcsModuleConfig & { contentTree?: any[] } = {
+        ...module?.config,
+      };
+      appConfig._id = 'catalogConfigWithLayers';
+      if (themes && themes.themes && appConfig) {
+        appConfig.layers = [
+          ...(appConfig.layers || []),
+          ...themes.themes[0].children[0].children.map((layer) => {
+            if (layer && layer.type) {
+              let layerConfig = {
+                id: layer.id,
+                name: layer.name,
+                source: layer.source,
+                style: layer.style,
+                layers: layer.name,
+                activeOnStartup: false,
+                allowPicking: false,
+                // featureNS: layer.featureNS,
+                // featurePrefix: layer.featurePrefix,
+                // featureType: layer.featureType,
+                // projection: {epsg: '3857'},
+                properties: layer.properties,
+                type: `${layer.type}Layer`,
+              };
+              // TODO: handle more layer types?
+              switch (layer.type) {
+                case 'WMS':
+                  layerConfig = {
+                    ...layerConfig,
+                    url: LUX_OWS_URL,
+                    // version: '1.3.0',
+                    tilingSchema: 'mercator',
+                    parameters: {
+                      format: 'image/png',
+                      transparent: true,
+                    },
+                  };
+                  break;
+                case 'WMTS':
+                  layerConfig = {
+                    ...layerConfig,
+                    url: `${LUX_WMTS_URL}/${layer.name}/GLOBAL_WEBMERCATOR_4_V3/{TileMatrix}/{TileCol}/{TileRow}.png`,
+                    // tilingSchema: 'mercator',
+                    extent: {
+                      coordinates: [-180, -85, 180, 85],
+                      projection: {
+                        epsg: 'EPSG:4326',
+                      },
+                    },
+                  };
+                  break;
+                default:
+                  break;
+              }
+              return layerConfig;
+            }
+            return null;
+          }),
+        ].filter((l) => l !== null);
+        if (appConfig.contentTree) {
+          appConfig.contentTree = [
+            ...(appConfig.contentTree || []),
+            ...themes.themes[0].children[0].children.map((layer) => {
+              if (layer && layer.type) {
+                return {
+                  name: layer.name,
+                  type: 'LayerContentTreeItem',
+                  layerName: layer.name,
+                };
+              }
+              return null;
+            }),
+          ].filter((l) => l !== null);
+        }
+
+        const newModule = new VcsModule(appConfig);
+        await vcsUiApp.addModule(newModule);
+        await vcsUiApp.removeModule(MODULE_ID);
+
+        console.log('App with new module', vcsUiApp);
+      }
+
       return Promise.resolve();
     },
     onVcsAppMounted(vcsUiApp: VcsUiApp): void {
